b841b2cf6bc5d23b4a0bdd9d49443beea9b257df
Rémi Siffert <remi.siffert@wanadoo.fr>
2012-07-17 23:24:33 +0200

    Added JUnit test plugin.

---
+++ padacore.test_plugin/src/padacore/test_plugin/AdaImplementationFileTest.java
@@ -0,0 +1,17 @@
+package padacore.test_plugin;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.padacore.AdaImplementationFile;
+
+public class AdaImplementationFileTest {
+
+	@Test
+	public void extensionTest() {
+		AdaImplementationFile sut = new AdaImplementationFile();
+
+		assertEquals("Extension of Ada implementation shall be set to ",
+				sut.getExtension(), "adb");
+	}
+}
---
+++ padacore.test_plugin/src/padacore/test_plugin/GprProjectTest.java
@@ -0,0 +1,40 @@
+package padacore.test_plugin;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+
+import org.junit.Test;
+import org.padacore.GprProject;
+
+public class GprProjectTest {
+
+	@Test
+	public void constructorTest() {
+
+		GprProject sut = new GprProject ("First_Project");
+	
+		assertEquals("Project name shall be set", sut.name(), "First_Project");
+		assertEquals("One Source dir shall be set", sut.sourcesDir().size(), 1);
+		assertEquals("Source dir shall be set to .", sut.sourcesDir().get(0), ".");
+		assertEquals("Obj dir shall be set to obj", sut.objectDir(), "obj");
+		assertEquals("Exe dir shall be set to exe", sut.execDir(), "exe");
+	}
+
+	
+	@Test
+	public void saveTest() {
+		GprProject sut = new GprProject("Test");
+
+		final String expectedDefaultProject = "project Test is\n"
+				+ "\tfor Source_Dirs use (\".\");\n"
+				+ "\tfor Object_Dir use \"obj\";\n" + "\tfor Exec_Dir use \"exe\";\n"
+				+ "end Test;";
+		ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+		sut.save(output);
+
+		assertEquals("Gpr project content", expectedDefaultProject, output.toString());
+	}
+}
+ 
