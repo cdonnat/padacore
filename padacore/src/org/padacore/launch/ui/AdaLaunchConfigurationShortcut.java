ec6b14824d0544531286841bb551d62838351f03
Rémi Siffert <remi.siffert@wanadoo.fr>
2012-09-30 19:00:31 +0200

    Launch configuration shortcut is now displayed only when an Ada project or a file in an Ada project is selected.

--- padacore/src/org/padacore/launch/ui/AdaLaunchConfigurationShortcut.java
+++ padacore/src/org/padacore/launch/ui/AdaLaunchConfigurationShortcut.java
@@ -1,13 +1,30 @@
 package org.padacore.launch.ui;
 
+import org.eclipse.core.resources.IFile;
 import org.eclipse.debug.ui.ILaunchShortcut;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IEditorPart;
 
 public class AdaLaunchConfigurationShortcut implements ILaunchShortcut {
+	
+	private boolean isOnlyOneElementSelected(IStructuredSelection selection) {
+		return selection.size() == 1;
+	}
 
 	@Override
 	public void launch(ISelection selection, String mode) {
+		if(selection instanceof IStructuredSelection) {
+			IStructuredSelection structuredSelection = (IStructuredSelection)selection;
+			
+			if(isOnlyOneElementSelected(structuredSelection)) {
+				Object selectedElt = structuredSelection.getFirstElement();
+				if (selectedElt instanceof IFile) {
+					IFile selectedFile = (IFile)selectedElt;
+					System.out.println("Selected file is: " + selectedFile.getFullPath());
+				}
+			}
+		}
 
 	}
 
