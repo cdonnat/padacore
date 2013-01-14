baeae9969cfb9c81e0102c790573a88d70e9f64e
Rémi Siffert <remi.siffert@wanadoo.fr>
2013-01-14 22:27:33 +0100

    AdaProjectFromGprWizard is now an Import wizard instead of a New wizard.

--- org.padacore.ui/src/org/padacore/ui/wizards/AdaProjectFromGprWizard.java
+++ org.padacore.ui/src/org/padacore/ui/wizards/AdaProjectFromGprWizard.java
@@ -6,27 +6,28 @@ import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
-import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IImportWizard;
 import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.wizards.datatransfer.ExternalProjectImportWizard;
 import org.padacore.core.ProjectBuilder;
 import org.padacore.core.gnat.FileGprProjectFactory;
 import org.padacore.core.gnat.GnatAdaProjectAssociationManager;
 import org.padacore.core.gnat.GprProject;
 
 /**
- * This class defines a wizard which enables user to create a new Eclipse
- * project with Ada nature from an existing GPR project file.
+ * This class defines a wizard which enables user to import an existing GPR
+ * project file as an Eclipse project.
  * 
  * @author rs
  * 
  */
-public class NewAdaProjectFromGprWizard extends Wizard implements INewWizard {
+public class AdaProjectFromGprWizard extends Wizard implements IImportWizard {
 
 	private AdaProjectFromGprCreationPage page;
 	private ProjectBuilder eclipseAdaProjectBuilder;
 
-	public NewAdaProjectFromGprWizard() {
-		setWindowTitle("New Ada Project from a GPR Project");
+	public AdaProjectFromGprWizard() {
+		setWindowTitle("Import existing GPR Project");
 		this.eclipseAdaProjectBuilder = new ProjectBuilder(
 				new GnatAdaProjectAssociationManager());
 	}
