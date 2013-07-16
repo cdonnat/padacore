d5cb0b203be784490c2bd9b6ab484cfc3d1b09c7
Rémi Siffert <remi.siffert@wanadoo.fr>
2013-07-16 21:53:52 +0200

    refs #24

--- org.padacore.core/src/org/padacore/core/utils/ProgramNotFoundException.java
+++ org.padacore.core/src/org/padacore/core/utils/ProgramNotFoundException.java
@@ -2,6 +2,13 @@ package org.padacore.core.utils;
 
 import java.io.IOException;
 
+/**
+ * This class defines an exception thrown when an external command fails because
+ * it cannot be found.
+ * 
+ * @author RS
+ * 
+ */
 public class ProgramNotFoundException extends Exception {
 
 	private static final long serialVersionUID = 942802465502510604L;
